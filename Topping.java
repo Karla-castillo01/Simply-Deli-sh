9eec5b3312ac1754a7916fa94fcb725ae7f4e51b
Karla Castillo <karlacastbg03@gmail.com>
2025-05-28 12:26:52 -0700

    Order.java created

---
+++ Topping.java
@@ -0,0 +1,28 @@
+public class Topping {
+    private final String name;
+    private final String type;
+    private final double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Topping(String name, String type, double price) {
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + type + "): $" + price;
+    }
+}
