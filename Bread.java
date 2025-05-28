f73ae853a1ade1eb4c1a1d8bd3e161e38a7685e5
Karla Castillo <karlacastbg03@gmail.com>
2025-05-28 12:26:37 -0700

    Bread.java created

---
+++ Bread.java
@@ -0,0 +1,23 @@
+public class Bread {
+    private String type;
+
+    public Bread(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
+
+
+
